8726054779df13a95adeebcf54512391cd048d71
Dylan Lewis <dyldlewis@gmail.com>
2019-04-12 13:07:20 -0700

    install and configure login for auth0

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import { Navbar, Button } from "react-bootstrap";
 import "./App.css";
 
 class App extends Component {
+  goTo(route) {
+    this.props.history.replace(`/${route}`);
+  }
+
+  login() {
+    this.props.auth.login();
+  }
+
+  logout() {
+    this.props.auth.logout();
+  }
+
+  componentDidMount() {
+    const { renewSession } = this.props.auth;
+
+    if (localStorage.getItem("isLoggedIn") === "true") {
+      renewSession();
+    }
+  }
+
   render() {
+    const token = this.props.auth.getIdToken();
+    console.log(token);
+    console.log(process);
+    const { isAuthenticated } = this.props.auth;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            This is a little something to assure myself I still remember how to
-            program :)
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Navbar>
+          <Navbar.Brand>
+            <a href="http://localhost:3000">Auth0 - React</a>
+          </Navbar.Brand>
+          <Button className="btn-margin" onClick={this.goTo.bind(this, "home")}>
+            Home
+          </Button>
+          {!isAuthenticated() && (
+            <Button
+              id="qsLoginBtn"
+              className="btn-margin"
+              onClick={this.login.bind(this)}
+            >
+              Log In
+            </Button>
+          )}
+          {isAuthenticated() && (
+            <Button
+              id="qsLogoutBtn"
+              className="btn-margin"
+              onClick={this.logout.bind(this)}
+            >
+              Log Out
+            </Button>
+          )}
+        </Navbar>
       </div>
     );
   }
---
+++ src/Callback/Callback.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Callback extends Component {
+  render() {
+    return (
+      <div>
+        <p>loading</p>
+      </div>
+    );
+  }
+}
+
+export default Callback;
